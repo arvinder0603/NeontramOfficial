2ce680495a5475536477678d473b280c69e7ce1f
Arvinder singh <singharvinder59395@gmail.com>
2023-08-16 01:16:13 +0530

    jhbe

---
+++ src/components/Navi.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import landpagepic from  "../assets/landpagepic.png"
+
+const Navi = () => {
+  return (
+    <div style={{ backgroundImage: `url(${landpagepic})` }} className='flex justify-between'>
+
+        <div className='m-6  flex justify-center' >
+            <h1 className='text-[25px] font-extrabold '>Neontram</h1>
+        </div>
+        <h1>jhdfb</h1>
+    </div>
+  )
+}
+
+export default Navi
---
+++ src/pages/LandPage.jsx
@@ -0,0 +1,14 @@
+import Navi from '@/components/Navi';
+import React from 'react';
+import landpage from '../assets/landpage.png';
+
+const LandPage = () => {
+  return (
+    <div className="h-screen bg-cover bg-center" >
+      <Navi/>
+      {/* Other content */}
+    </div>
+  );
+};
+
+export default LandPage;
