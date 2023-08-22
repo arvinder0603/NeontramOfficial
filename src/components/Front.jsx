d672cbbddfef3bfa47203af481ae59a1047e4bd4
Arvinder singh <singharvinder59395@gmail.com>
2023-08-23 00:47:42 +0530

    changes

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -6,9 +6,9 @@ import { AiFillCheckSquare } from "react-icons/ai";
 
 export const Front = () => {
   return (
-    <div className='grid h-[100vh] lg:grid-cols-2 lg:mt-0  mt-12'>
+    <div className='grid h-[100vh]  lg:grid-cols-2 lg:mt-0  mt-12'>
     
-    <div className='flex lg:hidden justify-center items-center'>
+    <div className='flex lg:hidden  justify-center items-center'>
     <Image src={doct} alt="Family" />
     </div>
     
