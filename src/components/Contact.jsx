fcaefd2c3767053f7dc8b2b7b9cf6f692683af16
Arvinder singh <singharvinder59395@gmail.com>
2023-09-05 16:14:18 +0530

    dffv

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -71,7 +71,7 @@ const Contact = () => {
               ></input>
             </div>
           </div> */}
-          <h1>sdkj</h1>
+       
           <div className="p-2 w-full">
             <button  onClick={submitHandler} className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Contact</button>
           </div>
