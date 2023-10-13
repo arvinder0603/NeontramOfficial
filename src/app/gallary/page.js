e4499c633ec9946fdd1739cd3c233792bbabf6c3
arvinder0603 <singharvinder59395@gmail.com>
2023-10-13 15:57:10 +0530

    finassl

--- src/app/gallary/page.js
+++ src/app/gallary/page.js
@@ -21,15 +21,15 @@ import Link from 'next/link'
 
 
 
-export const metadata = {
-  title: 'Neontram Galary',
-  icons:{
-     icon:"https://document-export.canva.com/_0MVI/DAFs8D_0MVI/7/thumbnail/0001.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAQYCGKMUHWDTJW6UD%2F20230907%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230907T070557Z&X-Amz-Expires=5377&X-Amz-Signature=70e8c9dbcbbb49f306f13248aaf34fc2f50ba15d70e6df79581dde405b743ebd&X-Amz-SignedHeaders=host&response-expires=Thu%2C%2007%20Sep%202023%2008%3A35%3A34%20GMT"
-  }
+// export const metadata = {
+//   title: 'Neontram Galary',
+//   icons:{
+//      icon:"https://document-export.canva.com/_0MVI/DAFs8D_0MVI/7/thumbnail/0001.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAQYCGKMUHWDTJW6UD%2F20230907%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230907T070557Z&X-Amz-Expires=5377&X-Amz-Signature=70e8c9dbcbbb49f306f13248aaf34fc2f50ba15d70e6df79581dde405b743ebd&X-Amz-SignedHeaders=host&response-expires=Thu%2C%2007%20Sep%202023%2008%3A35%3A34%20GMT"
+//   }
   
   
   
-}
+// }
 
 const page = () => {
   return (
